6e1077d90ce10da50d82f47efd19f5c0c01a7d4f
wedqas <162343330+wedqas@users.noreply.github.com>
2025-11-20 07:32:54 +0000

    Delete portfolio directory

--- portfolio/apps/app1-age.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-def calculated(bir):
-    today = date.today()
-    return ((today.year - bir.year) + ((today.month - bir.month) / 12) + ((today.day - bir.day) / 365.25))
-
-print("Enter date as(DD/MM/YYYY)", calculated(date(2001, 9, 30)), "years")
--- portfolio/try.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-   return render_template('index.html', script='js/script.js')
-
-if __name__=="__main__":
-   app.run(debug=True)
